a4d8d64bb26a4455ddc5d0439e03515a83d99223
Jiwon Kang <jiwonk42@gmail.com>
2017-12-07 17:55:55 -0800

    ouput a list of recipes with ngFor

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from './../recipe.model';
 export class RecipesListComponent implements OnInit {
   // Recipe[]: object array
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'),
+    new Recipe('Pizza', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIjfXTZRXdSvf5nycPPh4jQ89B4rxiyGY7TPjZPEfRJeaxfhXytg'),
+    new Recipe('Paella', 'This is simply a test', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/5/20/0/CCTUL104_Ultimate-Paella_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384541152950.jpeg')
   ];
 
   constructor() { }
